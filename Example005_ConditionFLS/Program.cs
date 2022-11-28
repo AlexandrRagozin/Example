27f35194c32e738f4ae16adddff813fa0f724a02
Alexandr <ragozzzzinalexandr@gmail.com>
2022-11-28 21:36:10 +0500

    add Example005_ConditionFLS If-Else

---
+++ Example005_ConditionFLS/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "александр")
+{
+    Console.WriteLine("Ура, это же Сана!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
